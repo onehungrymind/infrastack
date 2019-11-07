a2f067da7be79a5f2fb9c756d0df062dc0392226
aklukas <chris.take@venturplex.com>
2019-11-07 14:08:25 -0700

    chore(docker-command): add port flag

--- server/postgres.sh
+++ server/postgres.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-docker run --name infrastack-postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=public -d postgres:alpine
+docker run --name infrastack-postgres -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_DB=public -d postgres:alpine
